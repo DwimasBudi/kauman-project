bb830f944994ef796f86729514c6f8954a0ee09a
Dwimas Budi Sulistyo <dwimasbudi21@gmail.com>
2023-09-09 11:14:47 +0700

    Initial commit

---
+++ resources/views/dashboard/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--=== Coding by CodingLab | www.codinglabweb.com === -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="css/dashboard-style.css">
+     
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <title>Admin Dashboard Panel</title> 
+</head>
+<body>
+    @include('dashboard.layouts.sidebar')
+    <section class="dashboard">
+    @include('dashboard.layouts.header')
+    
+    @yield('container')
+    </section>
+
+    <script src="js/dashboard-script.js"></script>
+</body>
+</html>
