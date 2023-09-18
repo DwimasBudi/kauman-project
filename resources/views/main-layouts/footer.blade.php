6c05545b7dc7910ada786bba99e2cb4d5113c6e2
wisnu4 <wisnu_2005101106@mhs.unipma.ac.id>
2023-09-18 10:36:13 +0700

    logo

--- resources/views/main-layouts/footer.blade.php
+++ resources/views/main-layouts/footer.blade.php
@@ -1,33 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Primary Meta Tags -->
-    <title>Sekolah Kita</title>
-    <meta name="title" content="Sekolah Kita" />
-    <meta name="description" content="ini sekolah kita, dbs❤" />
-
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://sklh.dwimasbudi.my.id/" />
-    <meta property="og:title" content="Sekolah Kita" />
-    <meta property="og:description" content="ini sekolah kita, dbs❤" />
-    <meta property="og:image" content="https://sklh.dwimasbudi.my.id/img/2.jpg" />
-
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content="https://sklh.dwimasbudi.my.id/" />
-    <meta property="twitter:title" content="Sekolah Kita" />
-    <meta property="twitter:description" content="ini sekolah kita, dbs❤" />
-    <meta property="twitter:image" content="https://sklh.dwimasbudi.my.id/img/2.jpg" />
-
-    <title>Sekolah Kita</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/media.css">
-</head>
     <div class="container">
         <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
             <div class="col mb-3">
