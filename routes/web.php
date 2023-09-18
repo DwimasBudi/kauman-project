312d452a653bf62e2edb837aa09a33ddde5abe96
Dwimas Budi Sulistyo <dwimasbudi21@gmail.com>
2023-09-18 19:11:41 +0700

    perbaikan controller dashboard post

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 use Illuminate\Http\Request;
 
 /*
@@ -25,14 +26,14 @@ Route::get('/ip', function (Request $request) {
 Route::get('/testing', function () {
     return "wisnu";
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-})->middleware('auth');
+Route::resource('/dashboard', DashboardController::class)->middleware('auth');
+// Route::get('/dashboard', function () {
+//     return view('dashboard.index');
+// })->middleware('auth');
 // Route::get('/bug', [PostController::class, 'index']);
 Route::get('/', [PostController::class, 'index']);
 Route::get('/admin', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate'])->middleware('throttle:loginx');
 Route::get('/logout/', [LoginController::class, 'logout']);
-Route::resource('/dashboard/posts/', DashboardController::class)->middleware('auth');
-Route::get('/dashboard/posts/checkSlug/', [DashboardController::class, 'checkSlug']);
+Route::resource('/dashboard/posts/', DashboardPostController::class)->middleware('auth');
+Route::get('/dashboard/posts/checkSlug/', [DashboardPostController::class, 'checkSlug']);
