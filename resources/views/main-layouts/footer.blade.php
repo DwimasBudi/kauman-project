ca52db174bc01272f2b3578b477c16af762cbee1
Dwimas Budi Sulistyo <dwimasbudi21@gmail.com>
2023-11-29 22:32:28 +0700

    link footer

--- resources/views/main-layouts/footer.blade.php
+++ resources/views/main-layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="bg-light text-center text-lg-start">
-    <div class="text-center p-3" style="background-color: rgba(146, 99, 99, 0.2);"> © 2020 Copyright: <a class="text-dark" href="https://sdnkauman.com/">SDN Kauman</a>
+    <div class="text-center p-3" style="background-color: rgba(146, 99, 99, 0.2);"> © 2020 Copyright: <a class="text-dark" href="https://sdnkaumanmagetan.site/">SDN Kauman</a>
     </div>
 </footer>
